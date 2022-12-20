c36c29498b330e06e71df00d38293eab6c0163cf
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-20 17:29:11 -0500

    React and Firebase
    Setting up React and Firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxbFDqWkoteHzFTffVQFtX-bvq8TV-yBA",
+    authDomain: "clone-1f584.firebaseapp.com",
+    projectId: "clone-1f584",
+    storageBucket: "clone-1f584.appspot.com",
+    messagingSenderId: "1067047737117",
+    appId: "1:1067047737117:web:3802bd3dd7aa90dfec73f4"
+  };
