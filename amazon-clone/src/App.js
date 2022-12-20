52ef3becb3f9233c672300b95bc8edcd38790d01
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-20 18:13:37 -0500

    basics for nav bar

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Amazon clone</h1>
-      {/* Header */}
+      <Header></Header>
       {/* Home */}
     </div>
   );
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,9 +1,50 @@
-// use rfce
+// use rfce for quick render
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <img 
+        className='header_logo'
+        // this links to a website, which is not ideal for real life
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png">
+        </img>
+
+        <div className="header__search">
+            <input className="header__searchInput" type="text">
+                {/* Logo */}
+            </input>
+        </div>
+
+        <div className='header__nav'>
+            <div className="header__option">
+                 <span className='header__optionLineOne'>
+                    Hello Guest
+                 </span>
+                 <span className='header__optionLineTwo'>
+                    Returns
+                </span>
+            </div>
+            <div className="header__option">
+                <span className='header__optionLineOne'>
+                    Hello Guest
+                 </span>
+                 <span className='header__optionLineTwo'>
+                    & Orders
+                </span>
+            </div>
+
+            <div className="header__option">
+            <span className='header__optionLineOne'>
+                    Your
+                 </span>
+                 <span className='header__optionLineTwo'>
+                    Prime
+                </span>
+            </div>     
+        </div>
+    </div>
   )
 }
 
