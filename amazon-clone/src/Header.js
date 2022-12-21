cbe217dbd9ce64002ee0e87b9f3022b9d5e48067
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-12-20 19:04:02 -0500

    Navbar React and CSS
    finishing up navbar visuals

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
-
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
@@ -26,12 +26,12 @@ function Header() {
                     Hello Guest
                  </span>
                  <span className='header__optionLineTwo'>
-                    Returns
+                    Sign In
                 </span>
             </div>
             <div className="header__option">
                 <span className='header__optionLineOne'>
-                    Hello Guest
+                    Returns
                  </span>
                  <span className='header__optionLineTwo'>
                     & Orders
@@ -44,6 +44,14 @@ function Header() {
                  </span>
                  <span className='header__optionLineTwo'>
                     Prime
+                </span>
+            </div>
+
+            <div className="header__optionBasket">
+                <ShoppingBasketIcon>
+                </ShoppingBasketIcon>
+                <span className="header__optionLineTwo header__basketCount">
+                0
                 </span>
             </div>     
         </div>
